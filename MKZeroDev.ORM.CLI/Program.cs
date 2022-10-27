08ec59bd130cc51b26c770c754a5bd88f006dca6
Mahmud Koli <mahmudkolirpi@gmail.com>
2022-10-28 03:34:34 +0600

    build assembly

--- MKZeroDev.ORM.CLI/Program.cs
+++ MKZeroDev.ORM.CLI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using Microsoft.Build.Evaluation;
+using Microsoft.Build.Locator;
+using System.Diagnostics;
 using System.Reflection;
 using System.Text;
 
@@ -6,32 +8,17 @@ namespace MKZeroDev.ORM.CLI
 {
     internal class Program
     {
+        private static string _projDir = Directory.GetCurrentDirectory();
+        private static string _outputDir = @"bin\Debug";
+        private static bool _buildAssembly = true;
+        private static string _connStr = default!;
+
         static void Main(string[] args)
         {
-            //args = "orm database-update --conn Server=.\\SQLEXPRESS;Database=ORMCore;Trusted_Connection=true".Split(' ');
-
-            //foreach (Type type in new Type[] { typeof(ORMDatabase) })
-            //{
-            //    var ctor = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new Type[] { typeof(string) });
-            //    var ctors = type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
-
-            //    foreach (var ct in ctors)
-            //    {
-            //        Console.WriteLine($"Constructor {ct.Name} params {string.Join(", ", ct.GetParameters().Select(x => x.ParameterType.FullName))}");
-            //    }
-
-            //    if (ctor.GetParameters().Any())
-            //        Console.WriteLine($"Taking params {string.Join(", ", ctor.GetParameters().Select(x => x.ParameterType.FullName))}");
-            //    else Console.WriteLine("No params to take");
-
-            //    Console.WriteLine($"Database {type.ToString()} updating...");
-            //    var obj = (ORMDatabase)Activator.CreateInstance(type, new object[] { args[2] });
-            //    obj.DatabaseUpdate();
-            //    Console.WriteLine($"Database {obj.ToString()} has been updated successfully.");
-            //}
-
             try
             {
+                //Test();
+
                 if (args == null || args.Length == 0)
                 {
                     ShowGeneralHelp();
@@ -77,21 +64,20 @@ namespace MKZeroDev.ORM.CLI
                 if (args.Length > 1 && (args[1] == "database-update"))
                 {
                     var connStrIn = args.ToList().IndexOf("--conn");
-                    if (args.Length <= (connStrIn+1))
+                    if (args.Length <= (connStrIn + 1) || string.IsNullOrEmpty(_connStr = args[connStrIn + 1]))
                     {
                         ShowInvalidCommandError();
                         return;
                     }
 
-                    var connStr = args[connStrIn + 1];
-                    if (string.IsNullOrEmpty(connStr))
-                    {
-                        ShowInvalidCommandError();
-                        return;
-                    }
+                    //_connStr = args[connStrIn + 1];
+                    //if (string.IsNullOrEmpty(_connStr))
+                    //{
+                    //    ShowInvalidCommandError();
+                    //    return;
+                    //}
 
-                    Console.WriteLine(connStr);
-                    DatabaseUpdate(connStr);
+                    DatabaseUpdate();
                     return;
                 }
 
@@ -115,11 +101,21 @@ namespace MKZeroDev.ORM.CLI
             }
         }
 
+        static void Test()
+        {
+            _projDir = @"D:\Practice\MKZeroDev\MKZeroDev.ORM\MKZeroDev.ORMTest";
+            _outputDir = "bin\\Debug";
+            _connStr = @"Server=.\SQLEXPRESS;Database=ORMCore;Trusted_Connection=true";
+            _buildAssembly = true;
+            DatabaseUpdate();
+        }
+
         static void ShowError(string message, string? stackTrace = null, string? source = null)
         {
             var stringBuilder = new StringBuilder();
 
-            stringBuilder.AppendLine($"Something went wrong! {Environment.NewLine} {message}");
+            stringBuilder.AppendLine("Something went wrong!!!");
+            stringBuilder.AppendLine(message);
             stringBuilder.AppendLine();
 
             if (!string.IsNullOrEmpty(stackTrace))
@@ -197,54 +193,113 @@ namespace MKZeroDev.ORM.CLI
             Console.WriteLine(msg);
         }
 
-        static void DatabaseUpdate(string connStr)
+        static void DatabaseUpdate()
         {
-            var directory = Directory.GetCurrentDirectory();
-            var csprojFiles = Directory.GetFiles(directory, "*.csproj", SearchOption.TopDirectoryOnly);
-            var dllFilesName = csprojFiles.Select(csp => Path.GetFileNameWithoutExtension(csp) + ".dll").ToList();
+            var assemblies = BuildProjectAssembly(_projDir);
+            var ormDatabaseInheritedTypes = GetInheritedTypesFromAssemblies<ORMDatabase>(assemblies);
+
+            foreach (Type type in ormDatabaseInheritedTypes)
+            {
+                var ctor = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public, new Type[] { typeof(string) });
+                var ctors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+
+                foreach (var ct in ctors)
+                {
+                    Console.WriteLine($"Constructor {ct.Name} params {string.Join(", ", ct.GetParameters().Select(x => x.ParameterType.FullName))}");
+                }
+
+                if (ctor?.GetParameters().Any() ?? false)
+                    Console.WriteLine($"Taking params {string.Join(", ", ctor.GetParameters().Select(x => x.ParameterType.FullName))}");
+                else Console.WriteLine("No params to take");
+
+                Console.WriteLine($"Database {type.ToString()} updating...");
+                var obj = (ORMDatabase?)Activator.CreateInstance(type, new object[] { _connStr });
+                obj?.DatabaseUpdate();
+                Console.WriteLine($"Database {obj?.ToString()} has been updated successfully.");
+            }
+
+            Console.WriteLine("Database update completed.");
+        }
+
+        #region Build assembly project
+        static IList<Type> GetInheritedTypesFromAssemblies<T>(IList<Assembly> assemblies)
+        {
+            var inheritedTypes = new List<Type>();
+
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes().Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(typeof(T))).ToList();
+                inheritedTypes.AddRange(types);
+            }
+
+            return inheritedTypes;
+        }
+
+        static IList<Assembly> BuildProjectAssembly(string projDir)
+        {
+            var assemblies = new List<Assembly>();
+            var projFiles = Directory.GetFiles(projDir, "*.csproj", SearchOption.TopDirectoryOnly);
+
+            if (_buildAssembly)
+            {
+                foreach (var csprojFile in projFiles)
+                {
+                    BuildAssemblyMSBuildWithRegister(csprojFile, _outputDir);
+                }
+            }
+
+            var dllFilesName = projFiles.Select(csp => Path.GetFileNameWithoutExtension(csp) + ".dll").ToList();
 
             var dllFiles = new List<string>();
+            var dllDir = Path.Combine(projDir, _outputDir);
+
             foreach (var dllFileName in dllFilesName)
             {
-                var dfs = Directory.GetFiles(directory, dllFileName, SearchOption.AllDirectories);
+                var dfs = Directory.GetFiles(dllDir, dllFileName, SearchOption.AllDirectories);
                 dllFiles.AddRange(dfs);
             }
 
             foreach (var dllFile in dllFiles)
             {
-                // Reference assemblies should not be loaded for execution.  They can only be loaded in the Reflection-only loader context
                 Assembly assembly = Assembly.LoadFile(dllFile);
 
-                //var references = assembly.GetReferencedAssemblies();
-                //foreach (var reference in references)
-                //{
-                //    var assm = Assembly.Load(reference.FullName);
-                //}
+                if (!IsAssemblyDebugBuild(assembly)) continue;
 
-                var ormDatabaseInheritedTypes = assembly.GetTypes().Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(typeof(ORMDatabase))).ToList();
-
-                foreach (Type type in ormDatabaseInheritedTypes)
+                var refAssemblies = assembly.GetReferencedAssemblies();
+                foreach (var refAssembly in refAssemblies)
                 {
-                    var ctor = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public, new Type[] { typeof(string) });
-                    var ctors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
-
-                    foreach (var ct in ctors)
-                    {
-                        Console.WriteLine($"Constructor {ct.Name} params {string.Join(", ", ct.GetParameters().Select(x => x.ParameterType.FullName))}");
-                    }
-
-                    if (ctor?.GetParameters().Any()??false)
-                        Console.WriteLine($"Taking params {string.Join(", ", ctor.GetParameters().Select(x => x.ParameterType.FullName))}");
-                    else Console.WriteLine("No params to take");
-
-                    Console.WriteLine($"Database {type.ToString()} updating...");
-                    var obj = (ORMDatabase?)Activator.CreateInstance(type, new object[] { connStr });
-                    obj?.DatabaseUpdate();
-                    Console.WriteLine($"Database {obj?.ToString()} has been updated successfully.");
+                    var assm = Assembly.Load(refAssembly.FullName);
                 }
+
+                assemblies.Add(assembly);
             }
 
-            Console.WriteLine("Database update completed.");
+            return assemblies;
         }
+
+        static bool IsAssemblyDebugBuild(Assembly assembly)
+        {
+            return assembly.GetCustomAttributes(false).OfType<DebuggableAttribute>().Any(da => da.IsJITTrackingEnabled);
+        }
+
+        //https://learn.microsoft.com/en-us/visualstudio/msbuild/updating-an-existing-application?view=vs-2022#use-microsoftbuildlocator
+        static void BuildAssemblyMSBuildWithRegister(string projFile, string outputDir)
+        {
+            MSBuildLocator.RegisterDefaults();
+            DirectlyBuildAssemblyMSBuild(projFile, outputDir);
+        }
+
+        static void DirectlyBuildAssemblyMSBuild(string projFile, string outputDir)
+        {
+            var outputPath = Path.Combine(Path.GetDirectoryName(projFile) ?? string.Empty, outputDir);
+            Project project = new Project(projFile);
+
+            project.SetGlobalProperty("Configuration", "Debug");
+            project.SetGlobalProperty("Platform", "Any CPU");
+            project.SetGlobalProperty("OutputPath", outputPath);
+
+            project.Build();
+        }
+        #endregion
     }
 }
